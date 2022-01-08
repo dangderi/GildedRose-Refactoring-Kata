2edb442f5ae4e7a769648b6371be6e62cfa685aa
Daniel <skho591@aucklanduni.ac.nz>
2022-01-08 13:41:00 +1300

    Unnested for loop

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -3,7 +3,7 @@ const {Shop, Item} = require("../src/gilded_rose");
 describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
+    const items = gildedRose.updateQualityItem();
     expect(items[0].name).toBe("fixme");
   });
 });
--- js-jest/test/texttest_fixture.js
+++ js-jest/test/texttest_fixture.js
@@ -8,8 +8,9 @@ const items = [
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 47),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47),
+  new Item("Backstage passes to a TAFKAL80ETC selldate passed", 1, 49),
 
   // This Conjured item does not work properly yet
   new Item("Conjured Mana Cake", 3, 6),
@@ -23,5 +24,5 @@ for (let day = 0; day < days; day++) {
   console.log(`\n-------- day ${day} --------`);
   console.log("name, sellIn, quality");
   items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
-  gildedRose.updateQuality();
+  gildedRose.updateQualityItem();
 }
